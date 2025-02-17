09ca2d5e6f59fc582f23de429943425b2fef4637
Alex Yang <jyan8933@uni.sydney.edu.au>
2025-02-18 00:12:44 +1100

    Finish array and loops JS exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elements) {
+
+    for (const element of elements) {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === element) {
+                arr.splice(i, 1);
+                i--;
+            }
+        }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (typeof a !== "number" || typeof b !== "number") return "ERROR";
+    if (a < 0 || !Number.isInteger(a) || b < 0 || !Number.isInteger(b)) return "ERROR";
 
+    sum = 0;
+    high = Math.max(a, b);
+    low = Math.min(a, b);
+
+    for (let i = low; i <= high; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
